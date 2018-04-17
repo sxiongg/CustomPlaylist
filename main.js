d630819d10ba1b1ace333b01f2ef3f69776b959e
Sia X <sia.xiong@outlook.com>
2018-04-16 22:09:57 -0700

    Final JS Changes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $('document').ready(function () {
     $('#searchButton').click(function () {
         var _queryUrl = "http://ws.audioscrobbler.com/2.0/?method=track.getinfo&api_key=4d87768072faff4ea2d4a189cfbe7115&format=json&artist="
-        
+
         //Alerts if search fields are empty
         if (inputArtist.value.length == 0) {
             alert("Enter Artist");
@@ -52,7 +52,7 @@ $('document').ready(function () {
     // Append track object to the resultsContainer div
     var appendAPIResult = function (resultObj) {
         var returnMessage = "<h4>Is this the song you are looking for?</h4>";
-        var searchResult = '<div class="col-md-8"> <span class="trackInformation"> <p>Artist: ' + '<a href="' + resultObj.album. url + '" target="_blank">'+ resultObj.artist.name + '</a> </p> </span>';
+        var searchResult = '<div class="col-md-8"> <span class="trackInformation"> <p>Artist: ' + '<a href="' + resultObj.album.url + '" target="_blank">' + resultObj.artist.name + '</a> </p> </span>';
         searchResult += '<span class="trackInformation"> <p>Track: ' + resultObj.name + '</p></span>';
 
         var albumImgResult = "<div class='col-md-2'><img src='" + resultObj.album.image[1]['#text'] + "'</img></div>";
@@ -61,7 +61,7 @@ $('document').ready(function () {
         $('#resultsContainer').append(albumImgResult);
         $('#resultsContainer').append(searchResult);
 
-        // Create a button to add to the final playlist
+        // Create a button to add to playlist
         var addToPlaylistButton = '<button class="btn addToPlaylist pull-right">Add <span class="glyphicon glyphicon-plus"></span> </button>';
         $('#resultsContainer').append(addToPlaylistButton);
 
@@ -69,7 +69,7 @@ $('document').ready(function () {
         var albumName = "";
         var albumImg = "";
         var deleteButton = "<div class='btn col-md-1'><button class='btn btn-xs deleteButton'> <span class='glyphicon glyphicon-remove'></span> </button></div>"
-            
+
         // Album object found. Set the album and image
         if (resultObj.album != undefined) {
             albumName = '<span class="trackInformation"> Album Name: ' + resultObj.album.title + '</span></div>';
@@ -78,26 +78,25 @@ $('document').ready(function () {
         else {
             // No album. Display a blank image placeholder
             albumName = "<span class='trackInformation'> Album not found.</span></div>";
-            albumImg  = "<div class='col-md-2'> </div>";
+            albumImg = "<div class='col-md-2'> </div>";
         }
 
-        $('.addToPlaylist').click(function(resultObj){
+        $('.addToPlaylist').click(function (resultObj) {
             // Move the result to the final playlist
             searchResult = albumImg + searchResult + albumName;
-    
+
             // Removes prior search result
-            $('#resultsContainer').children().detach();  
-    
+            $('#resultsContainer').children().detach();
+
             // Get the number from the last item in the playlist div
             // If no itemCard divs exist, then start with #1, if itemCard divs exist, get last div child value & increment by 1
             var playlistNumber = getIndexForPlaylistItem();
-    
+
             // Create the itemCard div to hold each individual track
-            var itemCard = '<div class="row" id="itemCard' +  playlistNumber + '"></div>';
-    
+            var itemCard = '<div class="row" id="itemCard' + playlistNumber + '"></div>';
+
             // Place the sequence number into the numberIndex div for each itemCard
             var numberIndex = '<div class="numberIndex col-md-1">' + playlistNumber + '</div>';
-            var shareButton = '<button class="btn btn-primary pull-right" id="sendEmailButton">' + '<span class="glyphicon glyphicon-envelope"></span> Share </button>';
 
             // Append itemCard to the playlist div
             $('#playlist-custom').append(itemCard);
@@ -105,23 +104,31 @@ $('document').ready(function () {
                 'border': '1px solid black',
                 'border-radius': '3px',
                 'padding': '10px',
-                'background-color': 'rgba(190, 186, 186, 0.6)'
-            })
-            $('#column-2').append(shareButton);
+                'background-color': 'rgba(137, 136, 132, 0.5)'
+            });
+
+            // Display Send button
+            if ($('#playlist-custom').children().get(0) != undefined) {
+                $('#sendEmailButton').css({
+                    'font-family': 'Play',
+                    'margin-top': '10px',
+                    'display': 'block'
+                })
+            }
 
             // Append the number index to itemCard div
             $('#itemCard' + playlistNumber).append(numberIndex);
-    
+
             // Append the artist and track result to itemCard div
-            $('#itemCard'  + playlistNumber).append(searchResult);
+            $('#itemCard' + playlistNumber).append(searchResult);
 
             $('#itemCard' + playlistNumber).hover(
-                function() {
-                    $( this ).append( $( deleteButton ) );
+                function () {
+                    $(this).append($(deleteButton));
                     $('.deleteButton').unbind('click').click(deleteRow);
-                  }, function() {
-                    $( this ).find( 'div:last' ).remove();
-                  }
+                }, function () {
+                    $(this).find('div:last').remove();
+                }
             )
             // Clear out the artist and song textboxes
             $('#inputArtist').val('');
@@ -153,7 +160,7 @@ $('document').ready(function () {
 
             // Check if the playlist div has children. If there are children then reorder the numbering.
             if ($('#playlist-custom').children().get(0) != undefined) {
-                
+
                 // Reorder the numbers and itemCard IDs in the div
                 renumberPlaylist();
             }
@@ -162,13 +169,13 @@ $('document').ready(function () {
                     'border': '0px solid black'
                 })
             }
-        }     
+        }
     }
 
     function renumberPlaylist() {
         // Reorders the sequence and itemCard div IDs in the playlist div children
         var new_number = 1;
-        $($("#playlist-custom").children()).each(function() {
+        $($("#playlist-custom").children()).each(function () {
             // Reset the itemCard ID
             $(this).get(0).id = "itemCard" + new_number;
 
@@ -192,45 +199,45 @@ $('document').ready(function () {
             // Increment it by one and return it
             var latestNum = $('#playlist-custom div:last-child').children('.numberIndex').text();
             return Number(latestNum) + 1;
-        } 
+        }
     };
 
     // Sort the playlist div
     Sortable.create(playlist, {
         animation: 150,
-        onEnd: function() {
+        onEnd: function () {
             // When the sort is over, update the playlist sequence
             renumberPlaylist();
-        }  
+        }
     });
 
-    $(function () {  
-            $('#sendEmailButton').click(function (){
-                // Grab last id and make it a number
-                var lastSong = $('#playlist-custom div:last-child').children('.numberIndex').text();
-                var lastSong = Number(lastSong);
-                console.log(lastSong);
-                var emailBodyInfoText = "";
-    
-                // Loop through each number index and create var of song info
-                for(i = 0; i < lastSong; i++) {
-                    //grab text of spans (song information)
-                    var grabItemCard = $('.trackInformation').parent().parent();
-                    console.log(grabItemCard[i]);
-                    var grabSpans = $(grabItemCard[i]).children().children('.trackInformation').text();
-                    // place info into variable and add new variable each iteration
-                    emailBodyInfoText += (i + 1) + ')' + grabSpans + '%0A' + '%0A';
-                    console.log(emailBodyInfoText);
-                }
-                // Email
-                var email = "";
-                var playlistTitle = $('#playlist h2').text();
-                var subject = 'Playlist: ' + playlistTitle;
-                var emailBody = playlistTitle + '%0A' + '%0A' + emailBodyInfoText;
-                document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody;
-                $('#emailAddressInput').val('');
-            })
-      });
+    $(function () {
+        $('#sendEmailButton').click(function () {
+            // Grab last id and make it a number
+            var lastSong = $('#playlist-custom div:last-child').children('.numberIndex').text();
+            var lastSong = Number(lastSong);
+            console.log(lastSong);
+            var emailBodyInfoText = "";
+
+            // Loop through each number index and create var of song info
+            for (i = 0; i < lastSong; i++) {
+                //grab text of spans (song information)
+                var grabItemCard = $('.trackInformation').parent().parent();
+                console.log(grabItemCard[i]);
+                var grabSpans = $(grabItemCard[i]).children().children('.trackInformation').text();
+                // place info into variable and add new variable each iteration
+                emailBodyInfoText += (i + 1) + ')' + grabSpans + '%0A' + '%0A';
+                console.log(emailBodyInfoText);
+            }
+            // Email
+            var email = "";
+            var playlistTitle = $('#playlist h2').text();
+            var subject = 'Playlist: ' + playlistTitle;
+            var emailBody = playlistTitle + '%0A' + '%0A' + emailBodyInfoText;
+            document.location = "mailto:" + email + "?subject=" + subject + "&body=" + emailBody;
+            $('#emailAddressInput').val('');
+        })
+    });
 
 })
 
